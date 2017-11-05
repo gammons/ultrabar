82164e9f8cead91c769a38e45477d347ba79473d
Grant Ammons <gammons@gmail.com>
2017-11-05 17:41:24 -0500

    Harden up the module runner

--- system/module-runner.js
+++ system/module-runner.js
@@ -5,20 +5,23 @@ export default class ModuleRunner {
   constructor(config, store) {
     this.config = config
     this.store = store
+    this.update = this.update.bind(this)
   }
 
   startProcessing() {
-    this.config.right_modules.forEach(m => {
-      if (m.updateInterval) {
-        this.update(m)
-      }
-    })
+    (this.config.right_modules || []).forEach(this.update);
+    (this.config.left_modules || []).forEach(this.update);
+    (this.config.center_modules || []).forEach(this.update)
   }
 
   update(module) {
-    const result = execSync(`/home/grant/.config/ultrabar/${module.name}/update`).toString()
-    const ret = {type: constants.MODULE_UPDATE, name: module.name, result}
-    this.store.dispatch(ret)
-    setTimeout(this.update.bind(this, module), module.updateInterval * 1000)
+    if (module.name) {
+      const result = execSync(`/home/grant/.config/ultrabar/${module.name}/update`).toString()
+      const ret = {type: constants.MODULE_UPDATE, name: module.name, result}
+      this.store.dispatch(ret)
+    }
+    if (module.updateInterval) {
+      setTimeout(this.update.bind(this, module), module.updateInterval * 1000)
+    }
   }
 }
