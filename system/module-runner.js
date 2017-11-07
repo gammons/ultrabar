9375a45b6509ef3359559a6fc7351b6c438a8864
Grant Ammons <gammons@gmail.com>
2017-11-07 07:34:25 -0500

    WIP on getting system modules to update

--- system/module-runner.js
+++ system/module-runner.js
@@ -1,6 +1,8 @@
 import { execSync } from 'child_process'
 import * as constants from '../src/constants'
 
+const mods = {}
+
 export default class ModuleRunner {
   constructor(config, store) {
     this.config = config
@@ -15,11 +17,19 @@ export default class ModuleRunner {
   }
 
   update(module) {
+    let ret
     if (module.name) {
       const result = execSync(`/home/grant/.config/ultrabar/${module.name}/update`).toString()
-      const ret = {type: constants.MODULE_UPDATE, name: module.name, result}
-      this.store.dispatch(ret)
+      ret = {type: constants.MODULE_UPDATE, name: module.name, result}
+    } else if (module.system) {
+      if (typeof(mods[module.system]) === 'undefined') {
+        mods[module.system] = require(`./internal_modules/${module.system}`)
+      }
+      const result = mods[module.system].update()
+      ret = {type: constants.MODULE_UPDATE, name: module.system, result}
     }
+    this.store.dispatch(ret)
+
     if (module.updateInterval) {
       setTimeout(this.update.bind(this, module), module.updateInterval * 1000)
     }
