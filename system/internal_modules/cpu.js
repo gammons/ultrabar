4a4e7b85323cf8ae7b2be4fa7afc92fecced6ed8
Grant Ammons <gammons@gmail.com>
2017-11-07 07:39:43 -0500

    WIP

--- system/internal_modules/cpu.js
+++ system/internal_modules/cpu.js
@@ -1,3 +1,29 @@
 import os from 'os'
 
+const os = require('os')
+
+function delta() {
+  const cpus = os.cpus()
+
+  return cpus.map(cpu => {
+    const times = cpu.times
+    return {
+      tick: Object.keys(times).filter(time => time !== 'idle').reduce((tick, time) => { tick+=times[time]; return tick }, 0),
+      idle: times.idle,
+    }
+  })
+}
+
+let startMeasures = delta()
+setInterval(() => {
+  const endMeasures = delta()
+  const percentageCPU = endMeasures.map((end, i) => {
+    return ((end.tick - startMeasures[i].tick) / (end.idle - startMeasures[i].idle) * 100).toFixed(3) + '%'
+  })
+
+  console.log(percentageCPU.join(' '), '\n')
+
+  // reset
+  startMeasures = delta()
+}, 2000)
 
